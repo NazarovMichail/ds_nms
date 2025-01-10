9d332c35f6ade19ef0a235daab6af9b6ca4dc8d6
NazarovMichail <126945101+NazarovMichail@users.noreply.github.com>
2025-01-10 12:23:53 +0300

    corr setup.py

--- setup.py
+++ setup.py
@@ -2,22 +2,19 @@ from setuptools import setup, find_packages
 import chardet
 
 
-ENCODING = None
-def parse_requirements(filename, encoding=None):
-    if encoding is None:
-        with open(filename, 'r', encoding='utf-8') as file:
-            return file.read().splitlines()
-    with open(filename, 'r', encoding=ENCODING) as file:
-        return file.read().splitlines()
-
 def detect_encoding(filename):
     with open(filename, 'rb') as file:
         raw_data = file.read()
         result = chardet.detect(raw_data)
         return result['encoding']
 
+def parse_requirements(filename):
+    ENCODING = detect_encoding(filename)
+    with open(filename, 'r', encoding=ENCODING) as file:
+        return file.read().splitlines()
+
 filename = "requirements.txt"
-ENCODING = detect_encoding(filename)
+
 
 setup(
     name='ds_nms',
