26c2cc1fefea538a0ffd83824f077b38c5045cf8
NazarovMichail <126945101+NazarovMichail@users.noreply.github.com>
2025-01-10 10:34:17 +0300

    add setup

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 
 def parse_requirements(filename):
-    with open(filename, 'r') as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         return file.read().splitlines()
 
 setup(
-    name='my_ml_package',
+    name='ds_nms',
     version='0.1',
-    description='Package for data processing and machine learning model optimization',
-    author='Your Name',
+    description='Пакет для анализа данных и машинного обучения',
+    author='Назаров Михаил',
     packages=find_packages(),
     install_requires=parse_requirements('requirements.txt')
 )
