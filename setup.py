6a3458db26eb18cab4e8640a652c53cddb7b6251
NazarovMichail <126945101+NazarovMichail@users.noreply.github.com>
2025-01-10 11:33:12 +0300

    add setup+chatdet

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
 from setuptools import setup, find_packages
+import chardet
 
 
-def parse_requirements(filename):
-    with open(filename, 'r', encoding='utf-16') as file:
+ENCODING = None
+def parse_requirements(filename, encoding=None):
+    if encoding is None:
+        with open(filename, 'r', encoding='utf-16') as file:
+            return file.read().splitlines()
+    with open(filename, 'r', encoding=ENCODING) as file:
         return file.read().splitlines()
+
+def detect_encoding(filename):
+    with open(filename, 'rb') as file:
+        raw_data = file.read()
+        result = chardet.detect(raw_data)
+        return result['encoding']
+
+filename = "requirements.txt"
+ENCODING = detect_encoding(filename)
 
 setup(
     name='ds_nms',
