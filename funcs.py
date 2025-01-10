4b96fe2e847dc4553114eb3d6774576a8989c644
NazarovMichail <126945101+NazarovMichail@users.noreply.github.com>
2025-01-10 10:22:56 +0300

    add setup

--- funcs.py
+++ funcs.py
@@ -28,46 +28,10 @@ from sklearn.decomposition import PCA
 from sklearn.model_selection import TimeSeriesSplit
 
 
-def features_separate(df: pd.DataFrame, threshold: int) -> Tuple[List[str], List[str]]:
-
-    categorical_columns = []
-    numerical_columns = []
-    for column_name in df.columns:
-        if df[column_name].nunique() < threshold:
-            categorical_columns.append(column_name)
-        else:
-            numerical_columns.append(column_name)
-
-    return categorical_columns, numerical_columns
 
 
-def save_split_descr(df_initial: pd.DataFrame,
-                    initial_columns: List[str],
-                    target: pd.Series,
-                    target_name: str,
-                    store: str='data/',
-                    dir: str='split_description/'
-                    ) -> None:
 
-    FILE_PATH = f'{store}{dir}{target_name}_descr.pkl'
 
-    df = df_initial[initial_columns]
-
-    target_indexes = target.index
-    df_description = df.loc[target_indexes, :]
-
-    save_checker = False
-    for col_name in initial_columns:
-        check_true = target.to_numpy() == df_description[col_name]
-        if check_true.all():
-            df_description.to_pickle(FILE_PATH)
-            save_checker = True
-
-    if save_checker:
-        print(f'File {FILE_PATH} saved !')
-
-    else:
-        print('Something wrong!')
 
 
 def kolmog_smirn_test(sample_1: pd.Series,
---
+++ setup.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import subprocess
+
+
+def create_venv(venv_name=".venv"):
+    """
+    Создает виртуальное окружение.
+
+    Args:
+        venv_name (str): Имя папки для виртуального окружения. Defaults to '.venv'.
+    """
+    print("Создаем виртуальное окружение...")
+    subprocess.run([sys.executable, "-m", "venv", venv_name], check=True)
+    print(f"Виртуальное окружение создано в папке {venv_name}.")
+
+def activate_venv(venv_name=".venv"):
+    """
+    Возвращает путь к активатору виртуального окружения.
+
+    Args:
+        venv_name (str): Имя папки виртуального окружения. Defaults to 'venv'.
+
+    Returns:
+        str: Команда для активации виртуального окружения.
+    """
+    if os.name == "nt":  # Windows
+        return os.path.join(venv_name, "Scripts", "activate")
+    else:  # Linux/MacOS
+        return os.path.join(venv_name, "bin", "activate")
+
+def install_github_package(repo_url, branch="master", venv_name=".venv"):
+    """
+    Устанавливает пакет с GitHub в виртуальное окружение.
+
+    Args:
+        repo_url (str): URL репозитория GitHub.
+        branch (str): Имя ветки. Defaults to 'main'.
+        venv_name (str): Имя папки виртуального окружения. Defaults to 'venv'.
+    """
+    activate_command = activate_venv(venv_name)
+    pip_install_command = f"pip install git+{repo_url}@{branch}"
+
+    print("Активируем виртуальное окружение и устанавливаем пакет...")
+    subprocess.run(f"{activate_command} && {pip_install_command}", shell=True, check=True)
+    print("Пакет успешно установлен!")
+
+if __name__ == "__main__":
+
+    REPO_URL = "https://github.com/NazarovMichail/ds_nms.git"
+    BRANCH = "master"
+
+    # Создание виртуального окружения и установка пакета
+    VENV_NAME = ".venv"
+    create_venv(VENV_NAME)
+    install_github_package(REPO_URL, BRANCH, VENV_NAME)
